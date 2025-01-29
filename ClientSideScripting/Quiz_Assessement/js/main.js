607aa211204393f9876e3ac656579b2f139a25f3
Alan <robbalan72@tiscali.co.uk>
2025-01-29 23:20:43 +0000

    Updated

--- ClientSideScripting/Quiz_Assessement/js/main.js
+++ ClientSideScripting/Quiz_Assessement/js/main.js
@@ -1,11 +1,15 @@
 var i = 0;
 var txt = 'Hey There Cadet Freddy! Welcome to the Space Academy!';
 var speed = 50;
-
 function typeWriter(){
+    
+
+
     if(i < txt.length){
         document.getElementById('welcome_txt').innerHTML += txt.charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     }
+    document.getElementById('welcome_txt').style.margin= "5%";
 }
+
