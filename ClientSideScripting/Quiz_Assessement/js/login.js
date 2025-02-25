074ad24a405b5691b4df38880aa028d3c4385619
Alan <robbalan72@tiscali.co.uk>
2025-02-25 23:44:24 +0000

    25/02/25

---
+++ ClientSideScripting/Quiz_Assessement/js/login.js
@@ -0,0 +1,10 @@
+
+
+function check(){
+    let setUser = "minions";
+    let setPassword = "GCCrockets";
+
+    let getUser = document.getElementById('userName').value;
+    let getPassword = document.getElementById('userEmail').value;
+    
+}
--- ClientSideScripting/Quiz_Assessement/js/main.js
+++ ClientSideScripting/Quiz_Assessement/js/main.js
@@ -1,14 +1,12 @@
 var i = 0;
 var txt = 'Hey There Cadet Freddy! Welcome to the Space Academy!';
-var speed = 50;
+var speechspeed = 50;
 function typeWriter(){
     
-
-
     if(i < txt.length){
         document.getElementById('welcome_txt').innerHTML += txt.charAt(i);
         i++;
-        setTimeout(typeWriter, speed);
+        setTimeout(typeWriter, speechspeed);
     }
     document.getElementById('welcome_txt').style.margin= "5%";
 }
